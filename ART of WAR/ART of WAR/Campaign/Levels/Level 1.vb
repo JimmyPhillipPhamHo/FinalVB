a21b6478c5d175fddc06b447e600ff3419faf3f8
jh145216 <31322578+jh145216@users.noreply.github.com>
2017-12-18 11:26:08 -0600

    update level 1

--- ART of WAR/ART of WAR/Campaign/Levels/Level 1.vb
+++ ART of WAR/ART of WAR/Campaign/Levels/Level 1.vb
@@ -1,13 +1,53 @@
 ﻿Public Class Level_1
     Dim moveSpeed As Integer = 15
+    Dim isJumping As Boolean
     Private Sub Level_1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
         Select Case e.KeyCode
             Case Keys.Right
                 tmrRight.Start()
+            Case Keys.Left
+                tmrLeft.Start()
+            Case Keys.Up
+                tmrUp.Start()
+                isJumping = True
         End Select
     End Sub
 
     Private Sub tmrRight_Tick(sender As Object, e As EventArgs) Handles tmrRight.Tick
         picTest.Left += moveSpeed
+    End Sub
+
+    Private Sub Level_1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        Select Case e.KeyCode
+            Case Keys.Right
+                tmrRight.Stop()
+            Case Keys.Left
+                tmrLeft.Stop()
+            Case Keys.Up
+                tmrUp.Stop()
+                isJumping = False
+        End Select
+    End Sub
+
+    Private Sub tmrLeft_Tick(sender As Object, e As EventArgs) Handles tmrLeft.Tick
+        picTest.Left -= moveSpeed
+    End Sub
+
+    Private Sub tmrUp_Tick(sender As Object, e As EventArgs) Handles tmrUp.Tick
+        picTest.Top -= moveSpeed
+    End Sub
+
+    Private Sub tmrGameLogic_Tick(sender As Object, e As EventArgs) Handles tmrGameLogic.Tick
+        If picTest.Bounds.IntersectsWith(picAir.Bounds) Then
+            If isJumping = False Then
+                tmrGravity.Start()
+            End If
+        ElseIf picTest.Bounds.IntersectsWith(picGround.Bounds) Then
+            tmrGravity.Stop()
+        End If
+    End Sub
+
+    Private Sub tmrGravity_Tick(sender As Object, e As EventArgs) Handles tmrGravity.Tick
+        picTest.Top += moveSpeed
     End Sub
 End Class
